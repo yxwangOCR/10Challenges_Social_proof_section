88058a9e1786de0198543fb2a2cb6c47fbfed747
Yixuan WANG <wang.yixuan@live.fr>
2022-12-22 14:02:48 -0500

    Add hero text and rating styles

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,9 +5,9 @@ import Stars from "./Stars";
 const Rating = () => {
   return (
     <div className='rating'>
-      <Stars review='Rated 5 Stars in Reviews' />
-      <Stars review='Rated 5 Stars in Report Guru' />
-      <Stars review='Rated 5 Stars in BestTech' />
+      <Stars review='Rated 5 Stars in Reviews' className='stars' />
+      <Stars review='Rated 5 Stars in Report Guru' className='stars starTwo' />
+      <Stars review='Rated 5 Stars in BestTech' className='stars starThree' />
     </div>
   );
 };
--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Star from "../images/icon-star.svg";
 import "../sass/Stars.css";
 
-const Stars = ({ review }) => {
+const Stars = ({ review, className }) => {
   return (
-    <div className='stars'>
+    <div className={className}>
       <div>
         <span>
           <img src={Star} alt='rating star' />
